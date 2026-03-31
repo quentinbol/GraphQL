0cc1679cb2c5fc3272be9b720a93b1d0fa081c57
quentinbol <quentin.bollore@epitech.eu>
2026-03-31 19:51:52 -0400

    feat: add Apollo Client configuration for GraphQL

---
+++ frontend/src/config/apolloClient.ts
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+
+const httpLink = new HttpLink({
+  uri: '/graphql',
+})
+
+export const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: { fetchPolicy: 'cache-and-network' },
+  },
+})
