df61e5a2e64da2f0f6647c8180ab05847b44f82f
quentinbol <quentin.bollore@epitech.eu>
2026-03-31 21:59:17 -0400

    feat: add detailed comments to GraphQL type definitions for clarity

--- backend/src/schema/typeDefs.ts
+++ backend/src/schema/typeDefs.ts
@@ -1,4 +1,7 @@
 export const typeDefs = `#graphql
+  """
+  Represents a student in the system
+  """
   type Student {
     id: ID!
     name: String!
@@ -6,20 +9,41 @@ export const typeDefs = `#graphql
     academicStanding: String!
   }
 
+  """
+  Input type for creating a new student
+  """
   input CreateStudentInput {
     name: String!
     completedCredits: Int!
   }
 
+  """
+  Root Query type
+  """
   type Query {
+    """
+    Retrieve all students
+    """
     students: [Student!]!
 
+    """
+    Retrieve a single student by their ID
+    """
     student(id: ID!): Student
 
+    """
+    Get total count of students
+    """
     studentCount: Int!
   }
 
+  """
+  Root Mutation type
+  """
   type Mutation {
+    """
+    Add a new student to the system
+    """
     addStudent(input: CreateStudentInput!): Student!
   }
 `;
